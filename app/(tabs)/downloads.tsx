fa75d6ad9ee78f8b53165e314b54b9a170f75ac4
TwanAsselbergs <twanasselbergs@outlook.com>
2025-06-24 13:45:27 +0200

    Add placeholder pages

---
+++ app/(tabs)/downloads.tsx
@@ -0,0 +1,9 @@
+import { Text, View } from "react-native";
+
+export default function DownloadsScreen() {
+  return (
+    <View className="">
+      <Text>Downloads Page</Text>
+    </View>
+  );
+}
---
+++ app/(tabs)/search.tsx
@@ -0,0 +1,9 @@
+import { Text, View } from "react-native";
+
+export default function Search() {
+  return (
+    <View className="">
+      <Text>Search Page</Text>
+    </View>
+  );
+}
